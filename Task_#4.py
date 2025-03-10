51df8be7bdb1217282117459379d11bcac4b76c1
Amjad Alrefaee <amjadalrefaee741@gmail.com>
2025-03-10 23:12:06 +0300

    Delete Task_#4.py

--- Task_#4.py
+++
@@ -1,6 +0,0 @@
-Provider_Name= input("Write the names of providers (Separate each name with a comma): ")
-#Offers= float(input("Write the cost of offers (Separate each amount with a comma:) "))
-
-input_provider = Provider_Name.split()
-print(input_provider)
-
